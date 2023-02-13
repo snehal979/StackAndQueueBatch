6b8d27ae3d733d9cdbd2a502a2fbb29dd0cb7bdb
Snehal979 <bansodsnehal535@gmail.com>
2023-02-13 23:13:09 +0530

    [Snehal]Add: stack linked list add element

--- StackAndQueueBatch/Program.cs
+++ StackAndQueueBatch/Program.cs
@@ -4,7 +4,12 @@
     {
         public static void Main(string[] args)
         {
-
+            Console.WriteLine("Stack and Queue Linked List");
+            StackLinkedList stackLinkedList = new StackLinkedList();
+            stackLinkedList.Push(70);
+            stackLinkedList.Push(30);
+            stackLinkedList.Push(56);
+            stackLinkedList.Display();
         }
     }
 }
---
+++ StackAndQueueBatch/StackLinkedList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueueBatch
+{
+    public class StackLinkedList
+    {
+        Node headTop;
+        /// <summary>
+        /// Uc1 Push -Add data
+        /// </summary>
+        /// <param name="data"></param>
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            if (headTop == null)
+            {
+                newNode.next = null;
+            }
+            else
+            {
+                newNode.next = this.headTop;
+            }
+            headTop =newNode;
+            Console.WriteLine("added the stack value "+data);
+        }
+        /// <summary>
+        /// Display StackList
+        /// </summary>
+        public void Display()
+        {
+            Node temp = headTop;
+            if (temp ==null)
+            {
+                Console.WriteLine("Stacklist is empty");
+            }
+            while (temp !=null)
+            {
+                Console.WriteLine(temp.data +" ");
+                temp =temp.next;
+            }
+        }
+    }
+}
