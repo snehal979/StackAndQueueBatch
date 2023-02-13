0d8677a04738d3dcee00b10ad7511afc88cbbf45
Snehal979 <bansodsnehal535@gmail.com>
2023-02-13 23:40:49 +0530

    [Snehal]Add:Add Data in queue Linked List

---
+++ StackAndQueueBatch/QueueLinkedList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueueBatch
+{
+    public class QueueLinkedList
+    {
+        Node headTop;
+        /// <summary>
+        /// Uc1-Enqueue -Add data
+        /// </summary>
+        /// <param name="data"></param>
+        public void Equeue(int data)
+        {
+            Node newNode = new Node(data);
+            if (headTop == null)
+            {
+                headTop = newNode;
+            }
+            else
+            {
+                Node temp = headTop;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+            }
+            Console.WriteLine("{0} inserted into Linked List", newNode.data);
+        }
+        /// <summary>
+        /// Display
+        /// </summary>
+        public void Display()
+        {
+            Node temp = headTop;
+            if (temp ==null)
+            {
+                Console.WriteLine("stacklist is empty");
+            }
+            while (temp !=null)
+            {
+                Console.WriteLine(temp.data +" ");
+                temp =temp.next;
+            }
+        }
+    }
+}
