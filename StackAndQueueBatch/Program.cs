38d681d39a451f13c6354576a0790e0f3b142d8b
Snehal979 <bansodsnehal535@gmail.com>
2023-02-13 23:33:48 +0530

    [Snehal]Add:Delect,Peek and Size of the stack linked list

--- StackAndQueueBatch/Program.cs
+++ StackAndQueueBatch/Program.cs
@@ -6,10 +6,47 @@
         {
             Console.WriteLine("Stack and Queue Linked List");
             StackLinkedList stackLinkedList = new StackLinkedList();
-            stackLinkedList.Push(70);
-            stackLinkedList.Push(30);
-            stackLinkedList.Push(56);
-            stackLinkedList.Display();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("A]Stack Linked List 1.Push 2.Peek 3.Pop 4.Peek 5.Pop 6.Empty 7.Size 8.exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        stackLinkedList.Push(70);
+                        stackLinkedList.Push(30);
+                        stackLinkedList.Push(56);
+                        stackLinkedList.Display();
+                        break;
+                    case 2:
+                        stackLinkedList.peek();
+                        stackLinkedList.Display();
+                        break;
+                    case 3:
+                        stackLinkedList.Pop();
+                        stackLinkedList.Display();
+                        break;
+                    case 4:
+                        stackLinkedList.peek();
+                        break;
+                    case 5:
+                        stackLinkedList.Pop();
+                        break;
+                    case 6:
+                        stackLinkedList.Empty();
+                        break;
+                    case 7:
+                        stackLinkedList.Size();
+                        break;
+                    case 8:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid");
+                        break;
+                }
+            }
         }
     }
 }
--- StackAndQueueBatch/StackLinkedList.cs
+++ StackAndQueueBatch/StackLinkedList.cs
@@ -43,5 +43,59 @@ namespace StackAndQueueBatch
                 temp =temp.next;
             }
         }
+        /// <summary>
+        /// Uc2-Peek Top Value Of StackLinkedList
+        /// </summary>
+        public void peek()
+        {
+            if (headTop==null)
+            {
+                Console.WriteLine("list is empty");
+            }
+            Console.WriteLine("{0} is the top of the stack list", headTop.data);
+        }
+        /// <summary>
+        /// Uc2-PoP-Delect top value
+        /// </summary>
+        public void Pop()
+        {
+            if (headTop==null)
+            {
+                Console.WriteLine("list is empty");
+            }
+            Console.WriteLine("value is pop "+headTop.data);
+            headTop = headTop.next;
+        }
+        /// <summary>
+        /// IsEmpty
+        /// </summary>
+        public void Empty()
+        {
+            while (headTop!=null)
+            {
+                peek();
+                Pop();
+            }
+        }
+        /// <summary>
+        /// Size Of StackList
+        /// </summary>
+        /// <returns></returns>
+        public int Size()
+        {
+            Node temp = this.headTop;
+            int count = 0;
+            if (temp == null)
+            {
+                Console.WriteLine("linked list is empty");
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data+" ");
+                temp = temp.next;
+                count++;
+            }
+            return count;
+        }
     }
 }
